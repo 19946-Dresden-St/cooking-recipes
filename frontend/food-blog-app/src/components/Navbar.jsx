90940f1683f2d9f7fd07553a67014ce5b96b7176
Izïa <izia.crinier@gmail.com>
2025-12-12 23:06:12 +0100

    Add redirection when logged out

--- frontend/food-blog-app/src/components/Navbar.jsx
+++ frontend/food-blog-app/src/components/Navbar.jsx
@@ -1,22 +1,37 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import Modal from "./Modal.jsx";
 import InputForm from "./InputForm.jsx";
 import {NavLink} from "react-router-dom";
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
+
+    let token = localStorage.getItem("token");
+    const [isLogin, setIsLogin] = useState(token ? true : false);
+
+    useEffect(() => {
+        setIsLogin(token ? false : true);
+    }, [token])
+
     const checkLogin = () => {
-        setIsOpen(true);
+        if(token) {
+            localStorage.removeItem("token")
+            localStorage.removeItem("user")
+            setIsLogin(true)
+        } else {
+            setIsOpen(true);
+        }
     }
+
     return (
         <>
             <header>
                 <h2>Food Blog</h2>
                 <ul>
                     <li><NavLink to="/">Home</NavLink></li>
-                    <li><NavLink to="/myRecipe">My Recipes</NavLink></li>
-                    <li><NavLink to="/favRecipe">Favourites</NavLink></li>
-                    <li onClick={checkLogin}><p className="login">Login</p></li>
+                    <li><NavLink onClick={() => isLogin && setIsOpen(true)} to={ !isLogin ? "/myRecipe" : "/"}>My Recipes</NavLink></li>
+                    <li><NavLink onClick={() => isLogin && setIsOpen(true)} to={ !isLogin ? "/favRecipe" : "/"}>Favourites</NavLink></li>
+                    <li onClick={checkLogin}><p className="login">{ (isLogin) ? "Login" : "Logout" }</p></li>
                 </ul>
             </header>
             {
