bf0d493aa3bbb092e60cff4259e227d0565c8542
Izïa <izia.crinier@gmail.com>
2025-12-17 01:03:15 +0100

    Add create recipe

--- frontend/food-blog-app/src/pages/Home.jsx
+++ frontend/food-blog-app/src/pages/Home.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import foodRecipe from '../assets/cookies.jpeg'
 import RecipeItems from "../components/RecipeItems.jsx";
+import {useNavigate} from "react-router-dom";
 
 export default function Home() {
+
+    const navigate = useNavigate();
+
     return (
         <>
             <section className="home">
                 <div className="left">
                     <h1>Food Recipe</h1>
                     <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad commodi cumque dolores exercitationem fuga harum iusto laudantium magnam minima modi natus odit pariatur, quibusdam sunt tenetur voluptate, voluptatum. Est, provident.</h5>
-                    <button>Share your recipe</button>
+                    <button onClick={() => navigate("/addRecipe")} >Share your recipe</button>
                 </div>
                 <div className="right">
                     <img src={foodRecipe} width="320px" height="300px" alt="cookies picture"></img>
